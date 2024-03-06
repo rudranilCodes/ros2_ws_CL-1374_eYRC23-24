cac57561d989951e062bda470ab12c8a029e3a63
Rudranil Bose <111632156+rudranilCodes@users.noreply.github.com>
2024-03-06 16:40:36 +0530

    Update ebot_dock.py
    Dynamic params changing update

--- src/ebot_control/ebot_control/ebot_dock.py
+++ src/ebot_control/ebot_control/ebot_dock.py
@@ -8,6 +8,11 @@ from rclpy.executors import MultiThreadedExecutor
 from tf_transformations import euler_from_quaternion
 from ebot_docking.srv import DockSw  # Import custom service message
 #from geometry_msgs.msg import Twist  
+from rcl_interfaces.msg import SetParametersResult
+from rcl_interfaces.srv import SetParameters
+from rclpy.parameter import Parameter
+from rclpy.parameter import ParameterValue
+from rclpy.parameter import ParameterType 
 import cmd
 from std_msgs.msg import Int32  
 from usb_relay.srv import RelaySw                        
@@ -71,6 +76,23 @@ class USB_Relay(Node):
         future = ebot_attacher.call_async(req)
         rclpy.spin_until_future_complete(self, future)
 
+class Paramchange(Node):
+    def __init__(self):
+        super().__init__('Param_change_client')
+    def call_param_change(self,mode):
+        changer=self.create_client(SetParameters,"/global_costmap/global_costmap/set_parameters")
+        request=SetParameters.Request()
+        if mode == 0:
+            x=Parameter(name="robot_radius",value=0.20)
+        elif mode == 1:
+            x=Parameter(name="robot_radius",value=0.30)
+        request.parameters=[x.to_parameter_msg()]
+        future=changer.call_async(request)
+        rclpy.spin_until_future_complete(self,future)
+        response = future.result()
+        print(response.results)
+
+
 class Docking(Node):
     def __init__(self):
         super().__init__('Docking_client')
@@ -129,7 +151,7 @@ class MyBotNavigator(Node):
         self.navigator.waitUntilNav2Active()
 
 
-        ############################################# FOR RACK 3 #########################################################
+        ############################################# FOR RACK 3 DOCk #########################################################
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
@@ -185,9 +207,12 @@ class MyBotNavigator(Node):
         attach_detach = USB_Relay()
         attach_detach.run_usb(1,1)
 
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+        param=Paramchange()
+        param.call_param_change(mode=0)
+
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
 
         docking=Docking()
         docking.run_docker(float(rack3_xy_yaw[0]),float(rack3_xy_yaw[1]+0.10),float(rack3_xy_yaw[2]+0.10),'rack3',False)
@@ -195,6 +220,67 @@ class MyBotNavigator(Node):
         #time.sleep(5.0)
         flag=0
         self.publisher_function()
+
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = 'map'
+        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        goal_pose.pose.position.x = rack3_xy_yaw[0] +1.0 #0.5, 2.05, 0.0
+        goal_pose.pose.position.y = rack3_xy_yaw[1] +0.10
+        goal_pose.pose.orientation.z = r3[2]
+        goal_pose.pose.orientation.w = r3[3]
+
+        # sanity check a valid path exists
+        # path = navigator.getPath(initial_pose, goal_pose)
+
+        self.navigator.goToPose(goal_pose)
+
+
+        
+
+        i = 0
+        while not self.navigator.isTaskComplete():
+        ################################################
+        #
+        # Implement some code here for your application!
+        #
+        ################################################
+            self.publisher_function()
+        # Do something with the feedback
+            i = i + 1
+            feedback = self.navigator.getFeedback()
+            if feedback and i % 5 == 0:
+                print('Estimated time of arrival: ' + '{:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds)
+                    + ' seconds.')
+
+                # Some navigation timeout to demo cancellation
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    self.navigator.cancelTask()
+
+                # Some navigation request change to demo preemption
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
+    
+                    self.navigator.goToPose(goal_pose)
+
+
+                # Do something depending on the return code
+        result = self.navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            print('Goal succeeded!')
+        elif result == TaskResult.CANCELED:
+            print('Goal was canceled!')
+        elif result == TaskResult.FAILED:
+            print('Goal failed!')
+        else:
+            print('Goal has an invalid return status!')
+
+
+        param.call_param_change(mode=1)
+
+
+         ############################################# FOR RACK 3 UNDOCk #########################################################
+
+
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
@@ -203,9 +289,9 @@ class MyBotNavigator(Node):
         goal_pose.pose.orientation.z = rd3[2]
         goal_pose.pose.orientation.w = rd3[3]
         self.navigator.goToPose(goal_pose)
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.30"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.30"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
         i = 0
         while not self.navigator.isTaskComplete():
             ################################################
@@ -243,16 +329,66 @@ class MyBotNavigator(Node):
         else:
             print('Goal has an invalid return status!')
 
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
+        param.call_param_change(mode=0)
 
         docking.run_docker(float(rack3_dock_xy_yaw[0]+0.25),float(rack3_dock_xy_yaw[1]-0.20),float(rack3_dock_xy_yaw[2]+0.05),'rack3',True)
         attach_detach.run_usb(1,0)
         time.sleep(2.0)
         flag=1
         self.publisher_function()
-        #################################################FOR RACK 2##############################################################
+
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = 'map'
+        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        goal_pose.pose.position.x = rack3_dock_xy_yaw[0]-0.85
+        goal_pose.pose.position.y =rack3_dock_xy_yaw[1]-0.20
+        goal_pose.pose.orientation.z = rd3[2]
+        goal_pose.pose.orientation.w = rd3[3]
+        self.navigator.goToPose(goal_pose)
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.30"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
+        i = 0
+        while not self.navigator.isTaskComplete():
+            ################################################
+            #
+            # Implement some code here for your application!
+            #
+            ################################################
+
+            # Do something with the feedback
+            self.publisher_function()
+            i = i + 1
+            feedback = self.navigator.getFeedback()
+            if feedback and i % 5 == 0:
+                print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                    + ' seconds.')
+
+                # Some navigation timeout to demo cancellation
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    self.navigator.cancelTask()
+
+                # Some navigation request change to demo preemption
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
+                
+                    self.navigator.goToPose(goal_pose)
+
+        # Do something depending on the return code
+        result = self.navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            print('Goal succeeded!')
+        elif result == TaskResult.CANCELED:
+            print('Goal was canceled!')
+        elif result == TaskResult.FAILED:
+            print('Goal failed!')
+        else:
+            print('Goal has an invalid return status!')
+
+        
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
@@ -306,15 +442,19 @@ class MyBotNavigator(Node):
         else:
             print('Goal has an invalid return status!')
 
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.30"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+        param.call_param_change(mode=1)
+
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.30"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
+
+        #################################################FOR RACK 2 DOCk##############################################################
 
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        goal_pose.pose.position.x = rack2_xy_yaw[0]+1.0
-        goal_pose.pose.position.y = rack2_xy_yaw[1]-0.20
+        goal_pose.pose.position.x = rack2_xy_yaw[0]+0.8
+        goal_pose.pose.position.y = rack2_xy_yaw[1]-0.10
         goal_pose.pose.orientation.z = r2[2]
         goal_pose.pose.orientation.w = r2[3]
 
@@ -362,30 +502,29 @@ class MyBotNavigator(Node):
             print('Goal failed!')
         else:
             print('Goal has an invalid return status!')
+
+        param.call_param_change(mode=0)
     
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
 
         attach_detach.run_usb(1,1)
-        docking.run_docker(float(rack2_xy_yaw[0]), float(rack2_xy_yaw[1]-0.20),float(rack2_xy_yaw[2]-0.08),'rack2',False)   
+        docking.run_docker(float(rack2_xy_yaw[0]), float(rack2_xy_yaw[1]-0.10),float(rack2_xy_yaw[2]-0.05),'rack2',False)   
         flag=0
         self.publisher_function()
-    
-        
+
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        goal_pose.pose.position.x = rack2_dock_xy_yaw[0]
-        goal_pose.pose.position.y = rack2_dock_xy_yaw[1]-0.75
-        goal_pose.pose.orientation.z = rd2[2]
-        goal_pose.pose.orientation.w = rd2[3]
+        goal_pose.pose.position.x = rack2_xy_yaw[0]+0.8
+        goal_pose.pose.position.y = rack2_xy_yaw[1]-0.10
+        goal_pose.pose.orientation.z = r2[2]
+        goal_pose.pose.orientation.w = r2[3]
 
         # sanity check a valid path exists
         # path = navigator.getPath(initial_pose, goal_pose)
-        command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
-        output = subprocess.check_output(command, shell=True, text=True)
-        print(output)
+
         self.navigator.goToPose(goal_pose)
 
 
@@ -427,11 +566,77 @@ class MyBotNavigator(Node):
             print('Goal failed!')
         else:
             print('Goal has an invalid return status!')
+
+        param.call_param_change(mode=1)
+
+
+        #################################################FOR RACK 2 UNDOCk##############################################################
+    
+        
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = 'map'
+        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        goal_pose.pose.position.x = rack2_dock_xy_yaw[0]
+        goal_pose.pose.position.y = rack2_dock_xy_yaw[1]-0.75
+        goal_pose.pose.orientation.z = rd2[2]
+        goal_pose.pose.orientation.w = rd2[3]
+
+        # sanity check a valid path exists
+        # path = navigator.getPath(initial_pose, goal_pose)
+        # command="ros2 param set /global_costmap/global_costmap robot_radius 0.20"
+        # output = subprocess.check_output(command, shell=True, text=True)
+        # print(output)
+        self.navigator.goToPose(goal_pose)
+
+
+        
+
+        i = 0
+        while not self.navigator.isTaskComplete():
+        ################################################
+        #
+        # Implement some code here for your application!
+        #
+        ################################################
+            self.publisher_function()
+        # Do something with the feedback
+            i = i + 1
+            feedback = self.navigator.getFeedback()
+            if feedback and i % 5 == 0:
+                print('Estimated time of arrival: ' + '{:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds)
+                    + ' seconds.')
+
+                # Some navigation timeout to demo cancellation
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    self.navigator.cancelTask()
+
+                # Some navigation request change to demo preemption
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
+    
+                    self.navigator.goToPose(goal_pose)
+
+
+                # Do something depending on the return code
+        result = self.navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            print('Goal succeeded!')
+        elif result == TaskResult.CANCELED:
+            print('Goal was canceled!')
+        elif result == TaskResult.FAILED:
+            print('Goal failed!')
+        else:
+            print('Goal has an invalid return status!')
+
+        param.call_param_change(mode=0)
+
         docking.run_docker(float(rack2_dock_xy_yaw[0]),float(rack2_dock_xy_yaw[1]+0.10),float(rack2_dock_xy_yaw[2]+0.10),'rack2',True)
         attach_detach.run_usb(1,0)
         time.sleep(2.0)
         flag=1
         self.publisher_function()
+
+        param.call_param_change(mode=1)
         self.navigator.lifecycleShutdown()
         # sanity check a valid path exists
         # path = navigator.getPath(initial_pose, goal_pose)
